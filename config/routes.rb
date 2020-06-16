24c50ef5ceb0c958a8d4a5af1f5be76f8654b7b4
hirokinakada <dudu.hiccc@gmail.com>
2020-06-16 07:58:30 +0000

    hiroki.bootstrap1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :customers, controllers: {
     registrations: 'customers/registrations',
-    sessions     : 'customers/sessions'
+    sessions: 'customers/sessions'
   }
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlだよ
 
 end
