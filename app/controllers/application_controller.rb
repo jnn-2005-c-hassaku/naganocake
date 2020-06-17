6f4b6b36d04278fedbd991ba11c800625fb90722
tsuny221 <kingoftsuny@gmail.com>
2020-06-17 08:20:57 +0000

    [Update] admin_top

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,13 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 	devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name ,:first_name ,:last_name_kana ,:first_name_kana ,:postcode ,:address ,:phone_number ,:email ])
 	#sign_upの際にnameのデータ操作を許。追加したカラム。
 	end
+	def after_sign_in_path_for(resource)
+	  case resource
+	  when Admin
+	    admins_home_path
+	  when Customer
+	    root_path
+	  end
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
 	devise_for :admins, controllers: {
-	  sessions: 'admins/sessions'
+		sessions: 'admins/sessions'
 	}
-  devise_for :customers, controllers: {
-    registrations: 'customers/registrations',
-    sessions: 'customers/sessions'
-  }
-  root 'home#top'
+	devise_for :customers, controllers: {
+		registrations: 'customers/registrations',
+		sessions: 'customers/sessions'
+	}
+	root 'home#top'
+
+	resources :products
+	resources :customers
+	resources :cart_items
+	resources :orders
+	resources :shipping_addresses
+
+	namespace :admins do
+		get 'home' => 'home#top'
+		resources :customers
+		resources :products
+		resources :orders
+		resources :categories
+	end
 end
