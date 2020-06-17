8fdb2f891e0187b9e8197d47260d1d51404f8799
tsuny221 <kingoftsuny@gmail.com>
2020-06-17 07:18:58 +0000

    [Update] admin_login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+	devise_for :admins, controllers: {
+	  sessions: 'admins/sessions'
+	}
   devise_for :customers, controllers: {
     registrations: 'customers/registrations',
     sessions: 'customers/sessions'
