72559e6684113f76b7c248af03e33fa3e438af43
tsuny221 <kingoftsuny@gmail.com>
2020-06-20 09:30:46 +0000

    [Update] update quantity

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,9 +1,7 @@
 class CartItem < ApplicationRecord
+	validates :quantity, :numericality => { :greater_than => 0 }
 	belongs_to :customer
 	belongs_to :product
-
-
-
 
 	def subtotal(price,quantity)
 		quantity * (price * 1.10r).ceil
