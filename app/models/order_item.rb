735801411280db7430861bdb9b81fa6419e8c09e
tsuny221 <kingoftsuny@gmail.com>
2020-06-23 13:02:42 +0000

    [Update] order_items

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,7 @@
 class OrderItem < ApplicationRecord
-	belongs_to :order
-	belongs_to :product
+	belongs_to :order, optional: true
+	belongs_to :product, optional: true
+	def subtotal(price,quantity)
+		quantity * (price * 1.10r).ceil
+	end
 end
