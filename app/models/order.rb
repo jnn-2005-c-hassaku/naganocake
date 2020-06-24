349bb0b9df8d2120f8d9570e7f0a5f5159e9d1dc
hirokinakada <dudu.hiccc@gmail.com>
2020-06-24 10:18:39 +0000

    add.validates

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,13 @@ class Order < ApplicationRecord
 	has_many :order_items,  dependent: :destroy
 	accepts_nested_attributes_for :order_items, allow_destroy: true
 
+
+
+
+validates :pay_type,:address,:direction, presence: true
+validates :postcode, format: {with: /\A[0-9]{3}[0-9]{4}\z/}
+
+
 enum pay_type: { クレジットカード: 1, 銀行振り込み: 2 }
 enum buy_status:[:入金待ち, :入金確認, :発送準備中, :発送済]
 #勝手に0,1,2,3と数字が振り分けられる。
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,11 @@ class Product < ApplicationRecord
 	has_many :cart_items, dependent: :destroy
 	has_many :order_items, dependent: :destroy
 
+
+validates :name, presence: true
+validates :price, numericality: { only_integer: true }
+#数字だけ許可。数字以外、空白を許可しない
+
 	#税込価格表示
 	def price_with_tax(price)
 		(price * 1.10r).ceil
