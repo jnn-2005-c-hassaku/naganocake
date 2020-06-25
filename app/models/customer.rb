4da1b4dd72137737714e93cef4711eb674518493
vagrant <vagrant@localhost.localdomain>
2020-06-25 12:15:53 +0000

    validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,15 @@ class Customer < ApplicationRecord
 	devise :database_authenticatable, :registerable,
 		:recoverable, :rememberable, :validatable
 
+	validates :first_name,presence:true
+	validates :last_name,presence:true
+	validates :first_name_kana,presence:true
+	validates :last_name_kana,presence:true
+	validates :postcode, format: {with: /\A[0-9]{3}[0-9]{4}\z/}
+	validates :address,presence:true
+	validates :phone_number,presence:true
+
+
 	has_many :orders, dependent: :destroy
 	has_many :shipping_addresses, dependent: :destroy
 	has_many :cart_items, dependent: :destroy
